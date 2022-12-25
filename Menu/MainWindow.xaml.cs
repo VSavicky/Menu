2e6728303818c399489c2c8c0eb01ca713d21bd5
matvey <matveylazarev516@gmail.com>
2022-12-25 23:36:04 +0600

    2

--- Menu/MainWindow.xaml.cs
+++ Menu/MainWindow.xaml.cs
@@ -25,7 +25,14 @@ namespace Menu
         List<string> list1 = new List <string>() { "Бургер","Гамбургер","Чизбургер","Пицца","Суши","Крылышки","Острые крылышки","Нагетсы","Картошка фри","Шашлык"};
         List<string> list2 = new List<string>() { "Кола","Пепси","Лимонад","Вода","Спрайт","Фанта","Квас","Минералка"};
         List<string> list3 = new List<string>() { "Тар-тар", "Чесночный", "Кетчуп", "Майонез", "Сырный" };
-       
+
+        List<string[]> ls = new List<string[]> { 
+            new string[] { "Бургер", "580" },
+            new string[] { "Гамбургер", "690" },
+            new string[] { "Чизбургер", "700" },
+            new string[] { "Пицца", "890" },
+
+        };
         List<string> price1 = new List<string>() { "580", "690", "700", "890", "1620", "350", "490", "790", "490", "990" };
         List<string> price2 = new List<string>() { "590", "720", "500", "250", "620", "450", "560", "320" };
         List<string> price3 = new List<string>() { "380", "270", "345", "150", "150", "400" };
@@ -34,24 +41,80 @@ namespace Menu
         public MainWindow()
         {
             InitializeComponent();
-            lv1.ItemsSource = list1;
-            lv2.ItemsSource = list2;
-            lv3.ItemsSource = list3;
-            ShowPrice();
-
-
+            LoadData2(ls, lv1);
+            LoadData(list2, price2, lv2);
+            LoadData(list3, price3, lv3);
         }
-        public void ShowPrice()
+        //этот код писал НЕ МАТВЕЙ
+        private void AddToCart(object sender, MouseButtonEventArgs e)
         {
-            
-            MessageBox.Show(""+ lv1.Items[0] + " "+ price1[0]);
+            ListView lv = (ListView)sender;
 
+            List<string[]> ls = new List<string[]>();
+
+            string[] item = lv.SelectedItem.ToString().Split('-');
+            int count = 0;
+
+            if (lv4.Items.Count > 1)
+            {
+                for (int i = 0; i < lv4.Items.Count; i++)
+                {
+                    string[] s = lv4.Items[i].ToString().Split('-');
+                    ls.Add(s);
+                }
+
+                int index = ls.IndexOf(item);
+                MessageBox.Show(ls[index].ToString());
+
+                for (int i = 0; i < lv4.Items.Count; i++)
+                {
+                    if (ls[i][0] == item[0])
+                    {
+                        count++;
+
+                        lv4.Items[index] = $"{item[0]}-{count}";
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            else
+            {
+                lv4.Items.Add($"{item[0]}-{count}");
+            }
+
+
+            //if (lv4.Items.Contains(lv.SelectedItem.ToString().Split('-')[0]))
+            //{
+            //    int i = lv4.Items.IndexOf(lv.SelectedItem.ToString().Split('-')[0]);
+
+            //    int count;
+            //    int.TryParse(lv4.Items.ToString().Split('-')[1], out count);
+
+            //    lv4.Items[i] = $"{lv.SelectedItem} x{count++}";
+            //}
+            //else
+            //{
+            //    string s = $"{lv.SelectedItem.ToString().Split('-')[0]} - 1";
+            //    lv4.Items.Add(s);
+            //}
         }
-       
-
-
-
-
+        private void LoadData(List<string> names, List<string> price, ListView lv)
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                lv.Items.Add($"{names[i]} - {price[i]}");
+            }
+        }
+        private void LoadData2(List<string[]> data, ListView lv)
+        {
+            foreach (var item in data)
+            {
+                lv.Items.Add($"{item[0]}-{item[1]}");
+            }
+        }
     }
     //public void LoadFile()
     //{
